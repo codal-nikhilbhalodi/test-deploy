3b9ffe8030f23cd553694ac10b92316ebbdb50bf
codal-nikhilbhalodi <nikhilb@codal.com>
2024-04-02 19:10:05 +0530

    added static files

--- pollme/urls.py
+++ pollme/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls", namespace="accounts")),
     path("polls/", include("polls.urls", namespace="polls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
